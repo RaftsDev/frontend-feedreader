b504eee8b547b433a0a478fc0b5fd3a27d90c02d
Rafael <kubr2017@gmail.com>
2018-10-21 16:35:10 -0400

    started new suit (describe section)

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -67,6 +67,8 @@ $(function() {
 
 
   /* TODO: Write a new test suite named "The menu" */
+  describe('The menu', function() {
+
 
   /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
@@ -95,4 +97,5 @@ $(function() {
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+  });
 }());
