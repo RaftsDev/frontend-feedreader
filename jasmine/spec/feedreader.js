2279bd9d85a766674084ed3c8b8ac5e027eb1c00
Rafael <kubr2017@gmail.com>
2018-10-21 16:05:59 -0400

    Met rest of requirements of ESlinter.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -21,80 +21,77 @@ $(function() {
          * allFeeds in app.js to be an empty array and refresh the
          * page?
          */
-        it('are defined', function() {
-            expect(allFeeds).toBeDefined();
-            expect(allFeeds.length).not.toBe(0);
-        });
+    it('are defined', function() {
+      expect(allFeeds).toBeDefined();
+      expect(allFeeds.length).not.toBe(0);
+    });
 
 
-        /* TODO: Write a test that loops through each feed
+    /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-         it('All elements in allFeeds array have URL',function(){
-           let urlContext= true;
-           /** Check all elements in loop.
+    it('All elements in allFeeds array have URL', function() {
+      urlContext= true;
+      /** Check all elements in loop.
            *   if even one url propeties in elements not defined or empty
            *   variable urlContext turn to false **/
+      for (let i = 0; i < allFeeds.length; i++) {
+        if (!allFeeds[i].url) {
+          urlContext = false;
+        }
+        expect(urlContext).toBe(true);
+      }
+    });
 
-           for (let i = 0; i < allFeeds.length; i++){
-             if (!allFeeds[i].url) {
-               urlContext = false;
-             }
-             expect(urlContext).toBe(true);
-           }
-         });
 
-
-        /* TODO: Write a test that loops through each feed
+    /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
 
-         it('All elements in allFeeds array have Name',function(){
-           let nameContext= true;
-           /** Check all elements in loop.
+    it('All elements in allFeeds array have Name', function() {
+      let nameContext= true;
+      /** Check all elements in loop.
            *   if even one name properties in elements not defined or empty
            *   variable nameContext turn to false **/
 
-           for (let i = 0; i < allFeeds.length; i++){
-             if (!allFeeds[i].name) {
-               nameContext = false;
-             }
-             expect(nameContext).toBe(true);
-           }
-         });
-
-
+      for (let i = 0; i < allFeeds.length; i++) {
+        if (!allFeeds[i].name) {
+          nameContext = false;
+        }
+        expect(nameContext).toBe(true);
+      }
     });
+  });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+  /* TODO: Write a new test suite named "The menu" */
 
-        /* TODO: Write a test that ensures the menu element is
+  /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
 
-         /* TODO: Write a test that ensures the menu changes
+  /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+  /* TODO: Write a new test suite named "Initial Entries" */
 
-        /* TODO: Write a test that ensures when the loadFeed
+  /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+  /* TODO: Write a new test suite named "New Feed Selection" */
 
-        /* TODO: Write a test that ensures when a new feed is loaded
+  /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
